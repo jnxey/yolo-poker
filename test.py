f18d4a56b00c7b855d6dc7d60de236c8e3c535cc
Lee <846046245@qq.com>
2026-01-04 17:37:33 +0800

    feat: 基础训练

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import time
 
 # 加载预训练模型
 # model = YOLO('yolov8n.pt')
-model = YOLO('./chips-best8m.pt')
+model = YOLO('./runs/obb/train/weights/best.pt')
 
 # 单张图片推理
 t1 = time.time()
 results = model.predict(
     data='data-chips.yaml',
-    source='./t21.jpg',  # 确保路径存在，尽量避免中文目录
+    source='./t19.jpg',  # 确保路径存在，尽量避免中文目录
     conf=0.3,
     device='cpu',
     save=True,
@@ -19,5 +19,5 @@ print("YOLO耗时:", time.time()-t1)
 
 # 遍历结果
 for r in results:
-    boxes = r.boxes.xyxy.cpu().numpy()
+    boxes = r.obb.xyxyxyxy.cpu().numpy()
     print("检测框:", boxes)
